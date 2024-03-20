f06de658b53102f823b7cebe28d17d7b5ab199d9
narayanibokde9 <116104916+narayanibokde9@users.noreply.github.com>
2024-03-20 15:52:25 +0530

    Chat styling

--- website/app/chat/openchat/[chatId]/page.jsx
+++ website/app/chat/openchat/[chatId]/page.jsx
@@ -195,42 +195,45 @@ const GroupChat = ({ params }) => {
 						</div>
 					</div>
 				</div>
-				<div className="border border-white h-screen border-l-4">
+				<div class="border border-gray-300 h-screen flex flex-col">
 					{chat && <ChatHeader chat={chat} />}
-					<div className=" bg-gray-200 shadow-2xl">
-						<button className="btn btn-info m-2" onClick={fetchChats}>
+
+					<div class="bg-gray-200 shadow-md p-4">
+						<button class="btn btn-primary mr-4" onClick={fetchChats}>
 							Fetch Chats
 						</button>
 						{chatId && (
-							<button className="btn btn-info m-2" onClick={handleJoinGroup}>
+							<button class="btn btn-primary mr-4" onClick={handleJoinGroup}>
 								Join Group
 							</button>
 						)}
-						<button className="btn btn-info m-2" onClick={handleFetchHistory}>
+						<button class="btn btn-primary" onClick={handleFetchHistory}>
 							Fetch History
 						</button>
 					</div>
-					<div className="flex flex-col justify-between">
-						<div className="chat-history-container bg-cyan-500">
+
+					<div class="flex-grow overflow-y-auto bg-white">
+						<div class="chat-history-container p-4">
 							{history.length > 0 && (
 								<ChatHistory user={user} messages={history} />
 							)}
 						</div>
-						<div className="flex items-center border bg-white p-2">
-							<input
-								type="text"
-								placeholder="Type your message..."
-								value={message}
-								onChange={handleMessageChange}
-								className="flex-grow outline-none px-2 py-1"
-							/>
-							<button
-								onClick={sendMessageText}
-								className="ml-2 bg-blue-500 text-white font-semibold px-4 py-2 rounded hover:bg-blue-600 focus:outline-none"
-							>
-								Send
-							</button>
-						</div>
+					</div>
+
+					<div class="flex items-center border-t border-gray-300 bg-white p-4">
+						<input
+							type="text"
+							placeholder="Type your message..."
+							value={message}
+							onChange={handleMessageChange}
+							class="flex-grow outline-none px-4 py-2 rounded-l-md border border-gray-300 focus:outline-none"
+						/>
+						<button
+							onClick={sendMessageText}
+							class="bg-blue-500 text-white font-semibold px-6 py-2 rounded-r-md hover:bg-blue-600 focus:outline-none"
+						>
+							Send
+						</button>
 					</div>
 				</div>
 			</div>
